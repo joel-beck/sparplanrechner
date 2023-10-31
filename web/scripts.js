956edbf56c739324f0837a05303f84c4d618b91a
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-10-31 22:42:07 +0100

    Add Checkboxes and Boxes to Frontend (#8)

--- web/scripts.js
+++ web/scripts.js
@@ -1,7 +1,65 @@
 "use strict";
 
-// Define a function to sync input and range values
-function syncValues(id, isSlider) {
+import { injectHTMLFiles } from "./compile.js";
+
+/**
+ * Validates a number that's formatted in the German numbering system, ensuring it's within a given range.
+ *
+ * @param {string} inputValue - The number to validate, formatted as a German number.
+ * @param {number} min - The minimum acceptable value for the number.
+ * @param {number} max - The maximum acceptable value for the number.
+ * @returns {boolean} Whether the number is valid and within the specified range.
+ */
+function validateGermanNumberInRange(inputValue, min, max) {
+  const germanNumberRegex = /^(\d+|\d{1,3}(\.\d{3})*),\d{2}$/;
+  if (!germanNumberRegex.test(inputValue)) {
+    alert(
+      'Invalid format. The number must be in German format, e.g., "123.456,78"'
+    );
+    return false;
+  }
+  const standardNumber = parseFloat(
+    inputValue.replace(/\./g, "").replace(",", ".")
+  );
+  if (standardNumber < min || standardNumber > max) {
+    alert(
+      `The number must be between ${min.toLocaleString(
+        "de-DE"
+      )} and ${max.toLocaleString("de-DE")}`
+    );
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Validates form fields with German number formats.
+ *
+ * @param {string} formId - The ID of the form to validate.
+ * @param {string} elementId - The ID of the form element to validate.
+ * @param {number} min - The minimum acceptable value for the element.
+ * @param {number} max - The maximum acceptable value for the element.
+ */
+function validateForm(formId, elementId, min, max) {
+  const form = document.getElementById(formId);
+  const element = document.getElementById(elementId);
+
+  form.addEventListener("submit", function (event) {
+    const inputValue = element.value;
+
+    if (!validateGermanNumberInRange(inputValue, min, max)) {
+      event.preventDefault();
+    }
+  });
+}
+
+/**
+ * Synchronizes the values between number inputs and slider inputs.
+ *
+ * @param {string} id - The ID of the number input.
+ * @param {boolean} isSlider - Whether the function is called from slider input.
+ */
+export function syncValues(id, isSlider) {
   const numberInput = document.getElementById(id);
   const sliderInput = document.getElementById(`${id}Slider`);
 
@@ -12,37 +70,131 @@ function syncValues(id, isSlider) {
   }
 }
 
-// Attach event listeners when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  const formFields = [
-    "startCapital",
+/**
+ * Initializes slider and number input synchronization for form fields.
+ *
+ * @param {Array} formFields - An array containing IDs of form fields to synchronize.
+ */
+function syncSliders(formFields) {
+  formFields.forEach(function (field) {
+    const numberInput = document.getElementById(field);
+    const sliderInput = document.getElementById(`${field}Slider`);
+
+    if (numberInput) {
+      numberInput.addEventListener("input", function () {
+        syncValues(field, false);
+      });
+    }
+
+    if (sliderInput) {
+      sliderInput.addEventListener("input", function () {
+        syncValues(field, true);
+      });
+    }
+  });
+}
+
+/**
+ * Initializes a checkbox to enable or disable associated inputs.
+ *
+ * @param {string} checkboxId - The ID of the checkbox that will control the state of the fields.
+ * @param {Array} controlledIds - An array of IDs for fields that should be controlled by the checkbox.
+ */
+function initializeCheckboxToggle(checkboxId, controlledIds) {
+  const checkbox = document.getElementById(checkboxId);
+  const controlledElements = controlledIds.map((id) =>
+    document.getElementById(id)
+  );
+
+  const toggleDisabledState = () => {
+    const isChecked = checkbox.checked;
+    controlledElements.forEach((el) => {
+      el.disabled = !isChecked;
+    });
+  };
+
+  toggleDisabledState();
+  checkbox.addEventListener("change", toggleDisabledState);
+}
+
+/**
+ * Initializes Bootstrap tooltips for elements with data-bs-toggle="tooltip" attributes.
+ */
+function initializeTooltips() {
+  const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+}
+
+/**
+ * Auto-formats numeric inputs to German locale.
+ *
+ * @param {Array} formFields - An array containing IDs of form fields to format.
+ */
+function formatNumericInputs(formFields) {
+  formFields.forEach(function (field) {
+    const inputElement = document.getElementById(field);
+    const sliderElement = document.getElementById(`${field}Slider`);
+
+    const formatValue = (value) => {
+      value = value.replace(/[\D\s\._\-]+/g, "");
+      value = value ? parseInt(value, 10) : 0;
+      return value === 0 ? "" : value.toLocaleString("de-DE");
+    };
+
+    if (inputElement) {
+      inputElement.addEventListener("input", function () {
+        const selection = window.getSelection().toString();
+        if (selection !== "") {
+          return;
+        }
+        inputElement.value = formatValue(inputElement.value);
+      });
+    }
+
+    if (sliderElement) {
+      sliderElement.addEventListener("input", function () {
+        const displayElement = document.getElementById(`${field}Display`);
+        if (displayElement) {
+          displayElement.textContent = formatValue(sliderElement.value);
+        }
+      });
+    }
+  });
+}
+
+/**
+ * Attaches event listeners when the DOM is fully loaded.
+ */
+document.addEventListener("DOMContentLoaded", async function () {
+  const sliderFields = [
     "savingsRate",
-    "annualReturn",
     "years",
+    "annualReturnRate",
     "inflationRate",
     "takeoutRate",
     "tax",
   ];
+  const formFields = sliderFields.concat(["startCapital"]);
 
-  formFields.forEach(function (field) {
-    // Event listener for the number input
-    const numberInput = document.getElementById(field);
-    numberInput.addEventListener("input", function () {
-      syncValues(field, false);
-    });
+  // Inject HTML content into the page
+  await injectHTMLFiles();
 
-    // Event listener for the range slider
-    const sliderInput = document.getElementById(`${field}Slider`);
-    sliderInput.addEventListener("input", function () {
-      syncValues(field, true);
-    });
-  });
-});
+  syncSliders(sliderFields);
+  initializeTooltips();
 
-// Initialize all tooltips on the page
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
+  initializeCheckboxToggle("enableInflation", [
+    "inflationRate",
+    "inflationRateSlider",
+  ]);
+  initializeCheckboxToggle("enableTax", ["tax", "taxSlider"]);
+  initializeCheckboxToggle("enableTakeout", [
+    "takeoutRate",
+    "takeoutRateSlider",
+  ]);
+
+  // formatNumericInputs(formFields);
 });
