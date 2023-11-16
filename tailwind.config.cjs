251c75b1edc3b3dccf1b69feec6ade9c834e07f1
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-11-16 20:58:17 +0100

    start dark mode styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./web/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-text-light": "#A9A9A9",
+        "color-text-dark": "#D3D3D3",
+        "color-bg-light": "#F8FAFC",
+        "color-bg-dark": "#203447",
+        "color-accent-light": "#F56565",
+        "color-accent-dark": "#ED8936",
+        "color-form-light": "#A9A9A9",
+        "color-form-dark": "#E5E4E2",
+      },
+    },
     fontFamily: {
       sans: ["Lato", "Montserrat", "Roboto", "sans-serif"],
       serif: ["Merriweather", "serif"],
