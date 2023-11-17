b8b85647ad0870e1c3f3ab324ef1be7311a53343
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-11-17 16:45:10 +0100

    continued front end styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "color-text-light": "#A9A9A9",
+        "color-text-light": "#313131",
         "color-text-dark": "#D3D3D3",
         "color-bg-light": "#F8FAFC",
         "color-bg-dark": "#193549",
@@ -12,7 +12,7 @@ module.exports = {
         "color-accent-dark": "#F49608",
         "color-hover-light": "#CD5456",
         "color-hover-dark": "#C07941",
-        "color-form-light": "#A9A9A9",
+        "color-form-light": "#EFEFEF",
         "color-form-dark": "#E5E4E2",
       },
       fontFamily: {
