3020afe059f8a7517746cf29f38f0790c7a48277
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-11-18 22:38:34 +0100

    add dark mode toggle to ui

--- web/scripts.js
+++ web/scripts.js
@@ -202,94 +202,110 @@ function formatNumericInputs(formFields) {
   });
 }
 
-function switchTheme(darkIcon, lightIcon, isDarkmode) {
-  const switchToggle = document.querySelector("#themeToggle");
+function prefersSystemDarkmode() {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches;
+}
 
-  console.log(switchToggle.classList);
-
-  if (isDarkmode) {
-    switchToggle.classList.remove("bg-yellow-500", "-translate-x-2");
-    switchToggle.classList.add("bg-gray-700", "translate-x-2");
-
-    setTimeout(() => {
-      switchToggle.innerHTML = darkIcon;
-    }, 250);
-    return;
-  }
-
-  switchToggle.classList.add("bg-yellow-500", "-translate-x-2");
-  switchToggle.classList.remove("bg-gray-700", "translate-x-2");
+function showLightIcon(themeToggle, lightIcon) {
+  themeToggle.classList.add("bg-yellow-500", "-translate-x-2");
+  themeToggle.classList.remove("bg-gray-700", "translate-x-2");
 
   setTimeout(() => {
-    switchToggle.innerHTML = lightIcon;
+    themeToggle.innerHTML = lightIcon;
   }, 250);
 }
 
-function toggleTheme(darkIcon, lightIcon, isDarkmode) {
-  isDarkmode = !isDarkmode;
-  localStorage.setItem("isDarkmode", isDarkmode);
-  switchTheme(darkIcon, lightIcon, isDarkmode);
+function showDarkIcon(themeToggle, darkIcon) {
+  themeToggle.classList.add("bg-gray-700", "translate-x-2");
+  themeToggle.classList.remove("bg-yellow-500", "-translate-x-2");
+
+  setTimeout(() => {
+    themeToggle.innerHTML = darkIcon;
+  }, 250);
+}
+
+function setInitialTheme(themeToggle, darkIcon, lightIcon) {
+  if (prefersSystemDarkmode()) {
+    localStorage.setItem("isDarkmode", true);
+    showDarkIcon(themeToggle, darkIcon);
+  } else {
+    localStorage.setItem("isDarkmode", false);
+    showLightIcon(themeToggle, lightIcon);
+  }
+}
+
+function isDarkmode() {
+  return localStorage.getItem("isDarkmode") === "true";
+}
+
+function toggleTheme(themeToggle, darkIcon, lightIcon) {
+  if (isDarkmode()) {
+    localStorage.setItem("isDarkmode", false);
+    showLightIcon(themeToggle, lightIcon);
+    return;
+  }
+
+  localStorage.setItem("isDarkmode", true);
+  showDarkIcon(themeToggle, darkIcon);
 }
 
 /**
  * Attaches event listeners when the DOM is fully loaded.
  */
-document.addEventListener("DOMContentLoaded", async function () {
-  const sliderFields = [
-    "savingsRate",
-    "years",
-    "annualReturnRate",
-    "inflationRate",
-    "takeoutRate",
-    "tax",
-  ];
-  // const formFields = sliderFields.concat(["startCapital"]);
+function main() {
+  document.addEventListener("DOMContentLoaded", async function () {
+    const sliderFields = [
+      "savingsRate",
+      "years",
+      "annualReturnRate",
+      "inflationRate",
+      "takeoutRate",
+      "tax",
+    ];
+    // const formFields = sliderFields.concat(["startCapital"]);
 
-  // Inject HTML content into the page
-  // await injectHTMLFiles();
+    // Inject HTML content into the page
+    // await injectHTMLFiles();
 
-  syncSliders(sliderFields);
-  initializeTooltips();
+    syncSliders(sliderFields);
+    initializeTooltips();
 
-  initializeCheckboxToggle("inflationRateCheckbox", [
-    "inflationRate",
-    "inflationRateSlider",
-  ]);
-  initializeCheckboxToggle("taxCheckbox", ["tax", "taxSlider"]);
-  initializeCheckboxToggle("takeoutRateCheckbox", [
-    "takeoutRate",
-    "takeoutRateSlider",
-  ]);
+    initializeCheckboxToggle("inflationRateCheckbox", [
+      "inflationRate",
+      "inflationRateSlider",
+    ]);
+    initializeCheckboxToggle("taxCheckbox", ["tax", "taxSlider"]);
+    initializeCheckboxToggle("takeoutRateCheckbox", [
+      "takeoutRate",
+      "takeoutRateSlider",
+    ]);
 
-  initializeCheckboxBooleanValue("form", [
-    "inflationRateCheckbox",
-    "takeoutRateCheckbox",
-    "taxCheckbox",
-  ]);
+    initializeCheckboxBooleanValue("form", [
+      "inflationRateCheckbox",
+      "takeoutRateCheckbox",
+      "taxCheckbox",
+    ]);
 
-  const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
 </svg>`;
 
-  const lightIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    const lightIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
 </svg>`;
 
-  const switchToggle = document.querySelector("#themeToggle");
+    const themeToggle = document.querySelector("#themeToggle");
 
-  if (switchToggle) {
-    let isDarkmode = localStorage.getItem("isDarkmode") === "true";
+    setInitialTheme(themeToggle, darkIcon, lightIcon);
 
-    // Initialize theme based on stored preference
-    switchTheme(darkIcon, lightIcon, isDarkmode);
-
-    // Event listener for changing the theme
-    switchToggle.addEventListener("click", function () {
-      toggleTheme(darkIcon, lightIcon, isDarkmode);
+    // TODO: Button moves but theme does not actually change on click
+    // TODO: Button Movement is not yet symmetric from light to dark mode
+    themeToggle.addEventListener("click", function () {
+      toggleTheme(themeToggle, darkIcon, lightIcon);
     });
-  } else {
-    console.error("Theme toggle switch not found!");
-  }
 
-  // formatNumericInputs(formFields);
-});
+    // formatNumericInputs(formFields);
+  });
+}
+
+main();
