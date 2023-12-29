f736247f0b4b5f8ffc826dfd99661d636549ab89
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-12-29 14:20:04 +0100

    Deploy docker image on amd64
    * deploy docker image on amd64
    
    * set border color of horizontal line

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: "class",
-    content: ["./web/**/*.{html,js}"],
+    content: ["./web/**/*.{ts,tsx,js,jsx,html}"],
     theme: {
         extend: {
             colors: {
