ce71f952134a472894c1a5dd1d77c36529ea34ed
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2024-02-04 19:56:29 +0100

    Add configuration files + update UI colors
    * add justfile
    
    * split htmx template
    
    * Merge branch 'main' into just
    
    * update UI colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,22 +5,18 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "color-text-light": "#313131",
-                "color-text-dark": "#D3D3D3",
-                "color-bg-light": "#F8FAFC",
-                // "color-bg-dark": "#193549",
-                "color-bg-dark": "#112534",
-                // "color-accent-light": "#F56565",
-                // "color-accent-light": "#E05C5D",
-                "color-accent-light": "#89689E",
-                // TODO: Header Background: #89689E
-                // TODO: Slider Background: #A17AB9
-                // "color-accent-dark": "#F49608",
-                "color-accent-dark": "#DB8910",
-                "color-hover-light": "#CD5456",
-                "color-hover-dark": "#C07941",
-                "color-form-light": "#EFEFEF",
-                "color-form-dark": "#E5E4E2",
+                "color-text-light": "#313131", // Text color in light mode
+                "color-bg-light": "#F8FAFC", // Background color in light mode
+                "color-header-light": "#006AE4", // Header + Button background color in light mode
+                "color-accent-light": "#3089E4", // Accent color in light mode
+                "color-hover-light": "#0056CC", // Hover color in light mode
+                "color-form-light": "#EFEFEF", // Form field background in light mode
+                "color-text-dark": "#D3D3D3", // Text color in dark mode
+                "color-bg-dark": "#112534", // Background color in dark mode
+                "color-header-dark": "#DB8910", // Header + Button background color in dark mode
+                "color-accent-dark": "#E38F1A", // Accent color in dark mode
+                "color-hover-dark": "#C07941", // Hover color in dark mode
+                "color-form-dark": "#E5E4E2", // Form field background in dark mode
             },
             fontFamily: {
                 body: ["Nunito"],
