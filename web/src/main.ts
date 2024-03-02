c2b802d189652d9838ea46360c70a7db0cdd64fa
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2024-03-02 19:57:42 +0100

    update readme

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,32 +1,33 @@
 import mask from "@alpinejs/mask";
 import collapse from "@alpinejs/collapse";
+import intersect from "@alpinejs/intersect";
 import Alpine from "alpinejs";
 
-
 function initAlpine() {
-	document.addEventListener("alpine:init", () => {
-		Alpine.store("darkMode", {
-			on: window.matchMedia("(prefers-color-scheme: dark)").matches,
-			toggle() {
-				this.on = !this.on;
-			},
-		});
+    document.addEventListener("alpine:init", () => {
+        Alpine.store("darkMode", {
+            on: window.matchMedia("(prefers-color-scheme: dark)").matches,
+            toggle() {
+                this.on = !this.on;
+            },
+        });
 
-		Alpine.store("formula1", {
-			items: {
-				Ferrari: "Charles Leclerc",
-				Mercedes: "Lewis Hamilton",
-				RedBull: "Max Verstappen",
-			},
-		});
-	});
+        Alpine.store("formula1", {
+            items: {
+                Ferrari: "Charles Leclerc",
+                Mercedes: "Lewis Hamilton",
+                RedBull: "Max Verstappen",
+            },
+        });
+    });
 
-	Alpine.plugin(mask);
-	Alpine.plugin(collapse);
-	Alpine.start();
+    Alpine.plugin(mask); // eslint-disable-line
+    Alpine.plugin(collapse); // eslint-disable-line
+    Alpine.plugin(intersect); // eslint-disable-line
+    Alpine.start();
 }
 
-function main () {
+function main() {
     initAlpine();
 }
 
